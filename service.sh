423f7e355730ba906dd39fdbbb9770a27a91fcf8
Taka <takeru.kageyuki@gmail.com>
2022-08-27 16:28:28 +0700

    Added auto clean feature

--- service.sh
+++ service.sh
@@ -3,25 +3,25 @@
 # Declaring a function with the name "boot_completed"
 boot_completed() {
     # Check if the device is booted
-    while [ "$(getprop sys.boot_completed)" != "1" ]; do sleep 1; done
+    while [ $(getprop sys.boot_completed) != 1 ]; do sleep 1; done
     
     # Declaring a local variable with the name "file"
-    local file="/sdcard/.cache_cleaner"
+    local permission=/sdcard/.cache_cleaner
     # Create an empty file with the name ".cache_cleaner" in the "/sdcard" partition
-    touch $file
+    touch $permission
     # Check if the ".cache_cleaner" file is not in the "/sdcard" partition
-    while [ ! -f $file ]; do
+    while [ ! -f $permission ]; do
         # Create an empty file with the name ".cache_cleaner" in the "/sdcard" partition
-        touch $file
+        touch $permission
         # Pause script execution for 1 second
         sleep 1
     done
     # Delete the ".cache_cleaner" file in the "/sdcard" partition
-    rm $file
+    rm $permission
 }
 
 # Calling/load the "boot_completed" function
-boot_completed
+boot_completed &
 
 # Declaring a function with the name "cleaner"
 cleaner() {
@@ -37,8 +37,16 @@ cleaner() {
     find /sdcard/Android/data/*/cache/* -delete &>/dev/null
 }
 
-# Calling/load the "cleaner" function
-cleaner
+# Test Feature!
+auto_clean() {
+    while true; do
+        cleaner
+        sleep 6h
+    done
+}
+
+# Calling/load the "auto_clean" function
+nohup sh auto_clean &
 
 # Notes:
 # For more information about Shell Script, you can google it.
